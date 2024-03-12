c8bb01644287769af9b1708ee20df33214bfc352
Iain Attwater <ia@dbi-nyc.com>
2024-03-12 15:29:16 -0400

    Updated all pages

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Links from './links/Links'
+
+function NavBar() {
+  return (
+    <> 
+    <div>Logo</div>
+    <Links />
+    </>
+  ) 
+}
+
+export default NavBar
